9ac48c867de8eae13f5ecd8e0c5797db1a1c3017
Vincent Boucheny <mankalas@gmail.com>
2023-02-13 17:35:28 +1300

    Exif KISS

--- spec/exif_spec.rb
+++ spec/exif_spec.rb
@@ -6,11 +6,4 @@ RSpec.describe Exif do
       expect(flat_hash({a: 2, b: {c: {d: 3}}})).to eq({"a" => 2, "b_c_d" => 3})
     end
   end
-
-  describe "hash_to_insert" do
-    it "turns a hash into an INSERT instruction" do
-      expect(hash_to_insert({a: 3, r_t: 5, f_g: "123"}))
-        .to eq("INSERT INTO photos (a,r_t,f_g) VALUES ('3','5','123')")
-    end
-  end
 end
