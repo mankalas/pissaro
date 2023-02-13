a1536dc43745c132e68806cfdfde948be057d3b5
Vincent Boucheny <mankalas@gmail.com>
2023-02-13 17:50:33 +1300

    Add file mime and name info to snapshot

--- lib/pissaro/snapshot.rb
+++ lib/pissaro/snapshot.rb
@@ -1,6 +1,7 @@
 require 'digest'
 require 'pissaro/persistence'
 require 'pissaro/exif'
+require 'filemagic'
 
 class Snapshot
   attr_reader :persistence, :snap_id
@@ -29,12 +30,19 @@ class Snapshot
   def process_file(path)
     puts "Processing file #{path}"
     exif_hash = Exif.exif_to_hash(path)
-    media_data = { file_name: path, md5: md5(path), snapshot_id: snap_id }.merge(exif_hash)
+    media_data = { file_name: path, md5: md5(path), snapshot_id: snap_id }
+                   .merge(exif_hash)
+                   .merge(mime: mime(path))
+                   .merge(extname: File.extname(path), basename: File.basename(path), name: File.basename(path, File.extname(path)))
     persistence.insert_media(media_data)
   end
 
   def md5(path)
     Digest::MD5.hexdigest(File.read(path))
+  end
+
+  def mime(path)
+    FileMagic.new.file(path)
   end
 
   private
