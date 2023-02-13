e967957d92cb160cd44cbdd15e0f58c12db9586f
Vincent Boucheny <mankalas@gmail.com>
2023-02-13 18:09:59 +1300

    Fix Exif tests

--- spec/exif_spec.rb
+++ spec/exif_spec.rb
@@ -3,7 +3,7 @@ require "pissaro/exif"
 RSpec.describe Exif do
   describe "flat_hash" do
     it "concatenates nested hashes with '_'" do
-      expect(flat_hash({a: 2, b: {c: {d: 3}}})).to eq({"a" => 2, "b_c_d" => 3})
+      expect(Exif.flat_hash({a: 2, b: {c: {d: 3}}})).to eq({"a" => 2, "b_c_d" => 3})
     end
   end
 end
